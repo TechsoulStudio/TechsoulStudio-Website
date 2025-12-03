81981abdf92e02239697c2506edfff45277c12ad
TechsoulStudio <techsoulstudio@gmail.com>
2025-12-03 16:24:52 +0530

    add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -132,9 +132,31 @@ best branding company in India"
           strategy="beforeInteractive"
         />
       </head>
+
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NTCBH46H');
+        `,
+        }}
+      />
       <body
         className={`${inter.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NTCBH46H"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <LenisWrapper>{children}</LenisWrapper>
         <Toaster position="top-right" reverseOrder={false} />
       </body>
