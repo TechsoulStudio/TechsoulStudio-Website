13e9a4a14150c5682eb8b71aa47bdebfce01d15e
TechsoulStudio <techsoulstudio@gmail.com>
2025-12-03 14:48:54 +0530

    fix error

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,6 @@
 
 import Dashboard from "../components/dashboard/Dashboard";
 
-export const metadata = {
-  title: "TechsoulStudio",
-  description: "TechsoulStudio is a branding and web design agency based in Surat,India.",
-}
-
 export default function Home() {
   return (
    <main>
